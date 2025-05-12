76b7e1891a137fea7f69f00df9f4938b706132a0
Dinu <196576070+NewFrogCode@users.noreply.github.com>
2025-05-12 17:29:21 +0300

    Exercise number 1-5 working correctly

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
-const container = document.querySelector(`.container`)
 
-const createDivs = function(){
-    for(let i = 1; i <= 256 ; i++){
+const createDivs = function(divs){
+    const container = document.querySelector(`.container`)
+    container.innerHTML = ``
+    for(let i = 1; i <= divs*divs ; i++){
        const square = document.createElement(`div`)
+       let squareSize = 800 / divs;
+        square.style.width = `${squareSize}px`
+        square.style.height = `${squareSize}px`
        square.classList.add(`square`)
        container.appendChild(square)
     }
-    const square = document.querySelector(`.square`)
     container.addEventListener(`mouseover`, (e) =>{
         e.target.classList.add(`hover`)
     })
 }
+createDivs(16)
 
-createDivs()
+const button = document.querySelector(`.button`)
+button.addEventListener(`click`, (e)=>{
+    let answer = prompt(`Introduce the grid number`)
+    if(answer > 100) alert(`Number is WAY too high`)
+    else createDivs(answer)
+})
